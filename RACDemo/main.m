10f9d24747292cdf54e7008d5df7663d37efef38
mxmcc <4489990@qq.com>
2014-01-12 21:04:48 +0800

    Initial Commit

---
+++ RACDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  RACDemo
+//
+//  Created by wenlin on 14-1-12.
+//  Copyright (c) 2014年 bryq. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
